5621d35f651687fa3322a8c6ec3132d781b1ebd5
JustCode09 <Professionalacc011@gmail.com>
2025-08-04 16:36:41 +0545

    learned about raw function

--- express functions/express.js
+++ express functions/express.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON request bodies
-app.get('/', (req, res) => {
-    res.send('Hello World!'); // Respond with a simple message
+app.post('/greet', (req, res) => {
+    const name= req.body.name; // Extract name from request body
+    res.send(`Hello, ${name}`);
+   // Respond with a simple message
 });     
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000'); // Log server start
